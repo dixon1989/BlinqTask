accff4be1e84c4484900b004d9c852ace9fd2f76
lifepay-dickson <dickson.lim@lifepay.com.au>
2021-07-04 12:16:26 +0800

    finish up read me and finalise project

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # BlinqTask
 Blinq Task
+
+# README
+This README would normally document whatever steps are necessary to get your application up and running.
+
+# Tech Stack
+Hooks
+Typescript
+Next.js
+Axios
+React
+# How do I get set up?
+Be sure you have node.js installed.
+Make sure you are at the correct directory named ./BlinqTask.
+You will need to run "npm install" on your terminal.
+To view the history progress of my commits, run "git log" on your terminal.
+Run "npm run dev" to view the web application on my work.
+
+# Contribution guidelines
+Developing, Commiting code
+Code review
+Documentation
+
+# Code are deployed and develop personally by Dickson Lim
+Who do I talk to?
+Repo owner or admin - Dickson Lim
+Kindly email to dixon.limreload1989@gmail.com for any inquiry.
--- pages/index.tsx
+++ pages/index.tsx
@@ -8,6 +8,7 @@ import { useWindowSize } from '../components/UseWindowSize';
 
 export default function Home() {
 
+  // Hook state
   const [modalIsOpen, setIsOpen] = React.useState(false);
   const [errorMessage, setErrorMessage] = React.useState('')
   const [fullName, setfullName] = React.useState('');
@@ -32,11 +33,15 @@ let [winWidth] = useWindowSize();
     },
   };
 
+  // Validate email
+
   const validate = (email: string) => {
     const expression = /(?!.*\.{2})^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([\t]*\r\n)?[\t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([\t]*\r\n)?[\t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
 
     return expression.test(String(email).toLowerCase())
 }
+
+// Modal Control
 
   const openModal = () => {
     setIsOpen(true);
@@ -45,6 +50,8 @@ let [winWidth] = useWindowSize();
   const closeModal = () => {
     setIsOpen(false);
   }
+
+  // Actions
 
   const registerEmail = (event: { preventDefault: () => void }) => {
     event.preventDefault() 
@@ -65,14 +72,18 @@ let [winWidth] = useWindowSize();
   const onSubmit = () => {
     if (fullName && email && confirmEmail) {
       if (email === confirmEmail) {
-        setErrorMessage('')
-        setLoading(true)
-        setTimeout(
-          () => {
-            checkSampleData(fullName, confirmEmail)
-            setLoading(false)
-            setIsSuccess(true)
-          }, 3000)
+        if(validate(email)) {
+          setErrorMessage('')
+          setLoading(true)
+          setTimeout(
+            () => {
+              checkSampleData(fullName, confirmEmail)
+              setLoading(false)
+              setIsSuccess(true)
+            }, 3000)
+        } else {
+          setErrorMessage('Seems like one of your email inputs are not a valid email')
+        }
       } else {
         setErrorMessage('Email is not the same as Confirm email')
       }
